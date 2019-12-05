5a00bcfb923d8bd9b6567892962530361901db11
Shirley Ramirez <shirley.mramirez@yahoo.com>
2019-12-05 12:11:57 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import InputForm from './Components/InputForm/InputForm';
+import CSVReader from 'react-csv-reader'
+import './App.css';
+
+class App extends Component {
+  
+  render() { 
+    return ( 
+     <InputForm />
+    );
+  }
+}
+
+export default App;
+
+
+
